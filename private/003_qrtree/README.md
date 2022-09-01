8850dab599a9e7f6cdbc308582b9799ee598550e
Teque5 <teque5@gmail.com>
2022-08-31 17:13:33 -0700

    actually output img & hint info

--- private/003_qrtree/README.md
+++ private/003_qrtree/README.md
@@ -2,7 +2,17 @@
 
 QR Code reveals gort's hideout.
 
-## dependencies
+## Dependencies
 
 * `apt install qrencode`
 * `pip install pandas sklearn pillow`
+
+## Solutions
+
+### Flag 1
+
+**Flag{(-27.113, -109.350)}**
+
+### Hint for XX points
+
+*The length of the this binary classification output looks familiar...*
--- private/003_qrtree/solution_kyle_tree.py
+++ private/003_qrtree/solution_kyle_tree.py
@@ -7,6 +7,7 @@ Decision Tree Classifier
 import numpy as np
 import pandas as pd
 from sklearn import tree
+from PIL import Image
 
 
 BLAH = '../../public/003_qrtree/'
@@ -18,13 +19,17 @@ test_x = pd.read_csv(BLAH+'test_x_data.csv', index_col=False).values
 classifier = tree.DecisionTreeClassifier()
 classifier.fit(train_x, train_y)
 
-test_y_df = pd.DataFrame({'y': classifier.predict(test_x)})
+test_y = classifier.predict(test_x)
+test_y_df = pd.DataFrame({'y': test_y})
 #test_y_df.to_csv('/tmp/test_y_data.csv')
 #print('verify against /tmp/test_y_data.csv')
 
-test_y = pd.read_csv('test_y_data.csv', index_col=False).values.T[0]
+img = Image.fromarray(test_y.reshape(107, 97).astype(np.uint8)*255, mode='L').convert('1')
+img.save('qr_decode.png')
 
-accuracy = (classifier.predict(test_x) == test_y).sum() / (107*97)
+test_y_true = pd.read_csv('test_y_data.csv', index_col=False).values.T[0]
+
+accuracy = (test_y == test_y_true).sum() / (107*97)
 print(f'accuracy={accuracy:.4%}')
 
 print('done')
